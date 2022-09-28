006bcadb4eb61e93f5eb77fd645a723e5795b578
PachGit <108862443+PachGit@users.noreply.github.com>
2022-09-28 10:54:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /auto-filter-charlie
 fi
-cd /Ajax
+cd /auto-filter-charlie
 pip3 install -U -r requirements.txt
 echo "Starting 𝘾𝙝𝙖𝙧𝙡𝙞𝙚 𝘾𝙝𝙖𝙥𝙡𝙞𝙣....🔥"
 python3 bot.py
